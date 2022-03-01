5bf6ef0d798c608cba1f0783b2aafa7144f45479
Ackerven <ackerven@qq.com>
2022-03-01 15:19:30 +0800

    fix login API

--- src/main/java/erha/fun/demo/controller/UserController.java
+++ src/main/java/erha/fun/demo/controller/UserController.java
@@ -31,7 +31,8 @@ public class UserController {
 
     /**
      * 登陆接口
-     * @param map UserName Password role
+     *
+     * @param map      UserName Password role
      * @param response
      * @return
      */
@@ -41,38 +42,42 @@ public class UserController {
         User user = null;
         Integer role = Integer.parseInt(map.get("role"));
         if (role.equals(User.ADMIN)) {
-            user = new User();
+            user = null;
         } else if (role.equals(User.STUDENT)) {
             user = userService.queryStudentByUserName(map.get("username"));
         } else if (role.equals(User.TEACHER)) {
             user = userService.queryTeacherByUserName(map.get("username"));
         }
-
-        if (user != null && user.getPassword().equals(Tools.encryptToMD5(map.get("password")))) {
-            //Success
-            String token = TokenUtils.sign(user.getUsername());
-            Cookie cookie = new Cookie("token", token);
-            cookie.setMaxAge(7 * 24 * 60 * 60);
-            response.addCookie(cookie);
-            cookie = new Cookie("username", user.getUsername());
-            cookie.setMaxAge(7 * 24 * 60 * 60);
-            response.addCookie(cookie);
-            cookie = new Cookie("role", user.getRole().toString());
-            cookie.setMaxAge(7 * 24 * 60 * 60);
-            response.addCookie(cookie);
-            result.put("code", 200);
-            result.put("msg", "success");
-            result.put("token", token);
-            if (user.getRole().equals(User.ADMIN)) {
-                User u;
-            } else if (user.getRole().equals(User.STUDENT)) {
-                userService.addLoginRecord(((Student) user).getSid());
-            } else if (user.getRole().equals(User.TEACHER)) {
-                userService.addLoginRecord(((Teacher) user).getTid());
+        if (user != null) {
+            if (user.getPassword().equals(Tools.encryptToMD5(map.get("password")))) {
+                //Success
+                String token = TokenUtils.sign(user.getUsername());
+                Cookie cookie = new Cookie("token", token);
+                cookie.setMaxAge(7 * 24 * 60 * 60);
+                response.addCookie(cookie);
+                cookie = new Cookie("username", user.getUsername());
+                cookie.setMaxAge(7 * 24 * 60 * 60);
+                response.addCookie(cookie);
+                cookie = new Cookie("role", user.getRole().toString());
+                cookie.setMaxAge(7 * 24 * 60 * 60);
+                response.addCookie(cookie);
+                result.put("code", 200);
+                result.put("msg", "success");
+                result.put("token", token);
+                if (user.getRole().equals(User.ADMIN)) {
+                    User u;
+                } else if (user.getRole().equals(User.STUDENT)) {
+                    userService.addLoginRecord(((Student) user).getSid());
+                } else if (user.getRole().equals(User.TEACHER)) {
+                    userService.addLoginRecord(((Teacher) user).getTid());
+                }
+            } else {
+                result.put("code", 403);
+                result.put("msg", "用户名和密码错误");
             }
         } else {
             result.put("code", 403);
-            result.put("msg", "用户名和密码错误");
+            result.put("msg", "Role error");
         }
         return result;
     }
@@ -80,14 +85,15 @@ public class UserController {
 
     /**
      * 注册接口
-     * @param map username password role name
+     *
+     * @param map      username password role name
      * @param response
      * @return
      */
     @PostMapping("/signup")
     public Map<String, Object> signUp(@RequestBody Map<String, String> map, HttpServletResponse response) {
         Map<String, Object> result = new HashMap<>();
-        if(Integer.parseInt(map.get("role")) == 1) {
+        if (Integer.parseInt(map.get("role")) == 1) {
             User user;
         } else if (Integer.parseInt(map.get("role")) == 2) {
             Student student = new Student(map.get("username").toLowerCase(Locale.ROOT), map.get("password"), map.get("name"));
